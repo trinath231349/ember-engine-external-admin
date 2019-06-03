b7d297a6870ea33e17071d7bff02927c328f0949
trinath231349 <trinath.javaui@gmail.com>
2019-06-02 20:06:48 -0500

    temp changes

--- addon/routes/template-manager.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  dataStore: Ember.inject.service(),
-  model(){
-    return [this.get('dataStore').createRecord("post", {title: "My Post", desc: "My description of post"})];
-  }
-});
